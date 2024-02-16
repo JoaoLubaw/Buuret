3f26251dd7bebbbb8f616cdc3abc33ea2483f0b1
João Lubawaski <jlubawaski@gmail.com>
2024-02-16 19:48:46 -0300

    feat: pythonanyware // bug fix

--- backend/buser/views.py
+++ backend/buser/views.py
@@ -1,5 +1,4 @@
 from django.http import HttpResponse
-from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 
 import git
@@ -9,19 +8,14 @@ import git
 def update(request):
     if request.method == "POST":
         '''
-        pass the path of the diectory where your project will be
+        pass the path of the directory where your project will be
         stored on PythonAnywhere in the git.Repo() as parameter.
         Here the name of my directory is "test.pythonanywhere.com"
         '''
-        repo = git.Repo('/home/drsantos20/bookstore')
+        repo = git.Repo('/home/joaolubaw/Buuret/backend')
         origin = repo.remotes.origin
 
         origin.pull()
         return HttpResponse("Updated code on PythonAnywhere")
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere")
-
-
-def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
