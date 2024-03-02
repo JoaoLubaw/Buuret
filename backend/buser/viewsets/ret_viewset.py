37a4eb0d709c7cc8b7eb01b63a8849e870f26350
João Lubawaski <jlubawaski@gmail.com>
2024-03-02 11:19:59 -0300

    GitIgnore UPDATE

--- backend/buser/viewsets/ret_viewset.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from buser.models import Ret
-from buser.serializers import RetSerializer
-
-class RetViewSet(ModelViewSet):
-    serializer_class = RetSerializer
-    queryset = Ret.objects.all()
