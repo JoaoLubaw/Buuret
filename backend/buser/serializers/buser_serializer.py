80447dde6338be792377a19177c37f713ffdb68c
João Lubawaski <jlubawaski@gmail.com>
2024-03-09 18:50:51 -0300

    BuserSerializer

--- backend/buser/serializers/buser_serializer.py
+++ backend/buser/serializers/buser_serializer.py
@@ -9,10 +9,5 @@ class BuserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Buser
-<<<<<<< HEAD
-        fields = ['id', 'username', 'password', 'email', 'birthdate', 'telephone', 'name', 'description', 'followers', 'following', 'background', 'profile',
-                  'followers_count', 'following_count', 'rets_count', 'rets', 'buus_received', 'liked', 'buus_received_count']
-=======
         fields = ['id', 'username', 'email', 'name', 'birthdate', 'telephone', 'description',
                   'followers_count', 'following_count', 'rets_count', 'buus_received_count']
->>>>>>> c32869fc9c55c82b406381a79fd6abc829b1b9aa
