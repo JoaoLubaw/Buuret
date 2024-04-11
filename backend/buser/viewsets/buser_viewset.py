09b356a14a4402e0cce1d5ebec7fa1587675196b
João Lubawaski <jlubawaski@gmail.com>
2024-04-11 19:27:14 -0300

    busers --fix

--- backend/buser/viewsets/buser_viewset.py
+++ backend/buser/viewsets/buser_viewset.py
@@ -10,7 +10,6 @@ from django.db.models import Q
 from rest_framework import filters
 
 
-
 class IsFollowingOrReadOnly(permissions.BasePermission):
     """
     Permissão personalizada para seguir usuários.
@@ -30,23 +29,17 @@ class BuserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     lookup_field = 'username'
 
-    class BuserViewSet(ModelViewSet):
-        serializer_class = BuserSerializer
-        permission_classes = [IsAuthenticated]
-        lookup_field = 'username'
-
-        filter_backends = [filters.SearchFilter]
-        search_fields = ['username', 'name']
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['username', 'name']
 
     def get_queryset(self):
         username = self.kwargs.get('username', None)
         if username is not None:
             return Buser.objects.filter(username=username)
+        elif self.request.user.is_staff:
+            return Buser.objects.all()
         else:
-            if self.request.user.is_staff:
-                return Buser.objects.all()
-            else:
-                return Buser.objects.none()
+            return Buser.objects.none()
 
     @action(detail=False, methods=['get'])
     def search(self, request):
@@ -74,7 +67,7 @@ class BuserViewSet(ModelViewSet):
         user_to_follow = self.get_object()
         logged_buser = request.user
         if user_to_follow != logged_buser and not logged_buser.is_following(user_to_follow):
-            logged_buser.following.add(user_to_follow)  # Adicionar o usuário autenticado aos seguintes do usuário alvo
+            logged_buser.following.add(user_to_follow)  # Adicionar o usuário autenticado aos seguidores do usuário alvo
             user_to_follow.followers.add(logged_buser)  # Adicionar o usuário autenticado aos seguidores do usuário alvo
             user_to_follow.save()  # Salvar as mudanças no usuário alvo
             logged_buser.save()
@@ -109,5 +102,3 @@ class BuserViewSet(ModelViewSet):
 
         serializer = self.get_serializer(suggested_users, many=True)
         return Response(serializer.data)
-
-
